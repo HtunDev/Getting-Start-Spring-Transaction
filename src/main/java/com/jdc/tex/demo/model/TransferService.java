2fd9ea9c3b2bf56aab6237bd192667f7a21aabe7
hhtun137 <htunaunghlaing199@gmail.com>
2024-03-20 12:21:03 +0630

    writing class for transaction

---
+++ src/main/java/com/jdc/tex/demo/model/TransferService.java
@@ -0,0 +1,9 @@
+package com.jdc.tex.demo.model;
+
+import com.jdc.tx.demo.model.dto.TransferForm;
+import com.jdc.tx.demo.model.dto.TransferLog;
+
+public interface TransferService {
+
+	TransferLog transfer(TransferForm form);
+}
